b68ec7d932d474c02b5746e0f6d2caf79b579513
Abhishek Vishwakarma <abhishek.vishwakarma9@gmail.com>
2024-11-04 12:59:05 +0530

    chore (wip): overlord section

---
+++ src/modules/Home/overlord.tsx
@@ -0,0 +1,27 @@
+import { Stack, Text } from "@chakra-ui/react";
+import React from "react";
+
+import Card from "@/components/Card";
+
+function OverlordModule() {
+  // TODO: fetch data from api
+  return (
+    <Stack justifyContent="center" alignItems="center">
+      <Text color="red.600" fontWeight="bold" fontSize="1.5rem" padding="1rem">
+        AI Overlord
+      </Text>
+      <Card
+        image="https://bit.ly/dan-abramov"
+        createdBy="123456"
+        marketCap="32k"
+        createdAt={Date.now()}
+        replies="10"
+        name="Based frog"
+        ticker="$TYBF"
+        onClick={() => alert("coin clicked")}
+      />
+    </Stack>
+  );
+}
+
+export default OverlordModule;
