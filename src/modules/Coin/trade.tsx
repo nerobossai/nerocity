ab696afb10d98e62ba36eea380e9a5b5b8469784
AnushDeokar <deokaranush@gmail.com>
2024-11-12 14:11:52 +0530

    style: price and market cap text

--- src/modules/Coin/trade.tsx
+++ src/modules/Coin/trade.tsx
@@ -14,6 +14,7 @@ import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import React, { useEffect, useState } from "react";
 
+import SubscriptText from "@/components/SubscriptText";
 import { pumpFunSdk } from "@/services/pumpfun";
 import { logger } from "@/utils/Logger";
 
@@ -193,7 +194,7 @@ function TradeModule(props: TradeModuleProps) {
           fontSize="12px"
         >
           <Text>Price</Text>
-          <Text>${props.currentPrice}</Text>
+          <SubscriptText value={props.currentPrice} />
         </VStack>
         <VStack textAlign="center" fontWeight="bold" fontSize="12px">
           <Text>Holders</Text>
