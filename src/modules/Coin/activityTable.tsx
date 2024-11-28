b56310631884ff88e1d39dd2af719ea5d6663f87
AnushDeokar <deokaranush@gmail.com>
2024-11-28 14:05:03 +0530

    fix: activity bar

--- src/modules/Coin/activityTable.tsx
+++ src/modules/Coin/activityTable.tsx
@@ -18,6 +18,7 @@ import type { ActivityDetails } from "./services/coinApiClient";
 
 function ActivityTable({ activities }: { activities: ActivityDetails[] }) {
   const isMediumScreen = useBreakpointValue({ base: false, md: true });
+  const isLargeScreen = useBreakpointValue({base: false, lg: true});
   if (activities.length === 0) {
     return (
       <Box
@@ -34,48 +35,43 @@ function ActivityTable({ activities }: { activities: ActivityDetails[] }) {
   return (
     <VStack bg="#1B1B1E" width="100%" px="2rem">
       <Table as="table" width="100%">
-        <Thead fontSize="12px" color="#9B9B9B" textTransform="uppercase">
-          <Tr>
+        <Thead fontSize="12px" color="#9B9B9B" textTransform="uppercase" border="0">
+          <Tr borderBottom="0">
             <Th color="text.100" textAlign="left">
               Account
             </Th>
             <Th color="text.100" textAlign="left">
               Activity
             </Th>
-            <Th color="text.100" textAlign="right">
+            {isLargeScreen && <Th color="text.100">
               Value
-            </Th>
-            {isMediumScreen && <> <Th color="text.100" textAlign="right">
+            </Th>}
+            <Th color="text.100">
               SOL
             </Th>
-            <Th color="text.100" textAlign="right">
-              TXN
-            </Th></>}
           </Tr>
         </Thead>
         <Tbody>
           {activities.map((activity: ActivityDetails, id: number) => (
             <Box as="tr" key={id} display="table-row" mb="20px" fontSize="12px">
               <Td textAlign="left" color="white" border="0">
-                <Text>{activity?.username?.slice(0, 6) ?? "--"}</Text>
+                <Text>{activity?.user?.slice(0, 6) ?? "--"}</Text>
               </Td>
               <Td textAlign="left" color="white" border="0">
                 <Text>
+                  <span style={{color: activity.is_buy ?"#18CA2A" :"#D31341"}}>{activity.is_buy ? "BUY" : "SELL"}</span> &nbsp;
                   {timeDifference(
                     Date.now(),
                     parseInt((activity.timestamp*1000).toString(), 10),
                   )}
                 </Text>
               </Td>
-              <Td textAlign="right" color="white" border="0">
-                <Text>{activity.token_amount}</Text>
+              {isLargeScreen && <Td color="white" border="0">
+                <Text>{activity.token_amount/1000000}</Text>
+              </Td>}
+              <Td color="white" border="0">
+                <Text>{activity.sol_amount/1000000000}</Text>
               </Td>
-              {isMediumScreen && <><Td textAlign="right" color="white" border="0">
-                <Text>{activity.sol_amount}</Text>
-              </Td>
-              <Td textAlign="right" color="creator" border="0">
-                <Text>{activity.tx_index}</Text>
-              </Td></>}
             </Box>
           ))}
         </Tbody>
