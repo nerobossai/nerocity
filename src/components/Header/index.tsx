e7565e882c29871abf88811d76fe9530f89c3cf7
AnushDeokar <deokaranush@gmail.com>
2024-12-05 11:43:25 +0530

    fix: market cap

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -38,6 +38,11 @@ function formatToShortLink(number: any) {
   if (number >= 1_000_000) {
     return (number / 1_000_000).toFixed(1) + "M";
   }
+  
+  if (number >= 1_000) {
+    return (number / 1_000).toFixed(1) + "K";
+  }
+
   return number?.toString();
 }
 
